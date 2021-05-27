9dff2fe814e3feeb3ac6cfa4dca05dcb4938d64c
Igor M. Biondo <igormirko.biondo@gmail.com>
2021-05-27 11:44:32 +0200

    remove useless things

--- BlockChainClient/Program.cs
+++ BlockChainClient/Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Configuration;
 using System.Threading.Tasks;
 
 namespace BlockChainClient
